86545ce46fca964e1e5a2cd6d6170e08575ab84c
CryptoSamurai <therealcryptosamurai@gmail.com>
2021-04-07 21:04:52 +0200

    don't let ppl without password login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -14,16 +14,14 @@ class AuthController extends Controller {
         // Filter user from the users array by username and password
         const user = await UserRepository.findByColumn('wallet', wallet);
 
-        if (user.password !== PasswordHelper.getHashedPassword(password)) {
+        if (!user.password || user.password !== PasswordHelper.getHashedPassword(password)) {
             this.sendResponse(res, null, "Username or password incorrect");
             return;
         }
 
         // Generate an access token
         const accessToken = jwt.sign({wallet: wallet}, JWT_SECRET, {expiresIn: "2d"});
-        this.sendResponse(res, {
-            accessToken
-        });
+        this.sendResponse(res, { accessToken });
     }
 }
 
