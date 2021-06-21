585e452354e792d5975099d6f640390622735e65
Jay Welsh <thealchemicalopus@gmail.com>
2021-06-21 13:16:47 +0200

    fix:(ClaimController): email notifications env vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,3 @@ chmod 600 ./deploy.key
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 touch .env
 echo "$PRODUCTION_ENV_VARS" > .env
-echo "$MAILGUN_DOMAIN" >> .env
-echo "$MAILGUN_API_KEY" >> .env
-echo "$CLAIMS_ADMIN_RECIPIENTS" >> .env
