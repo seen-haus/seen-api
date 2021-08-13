4340111d837e057b72508329dfc77d5a53755934
Jay Welsh <thealchemicalopus@gmail.com>
2021-08-13 22:55:55 +0200

    fix(email-preferences-db-transformers): use logic compatible with older node version

--- src/transformers/user_email_preferences/index.js
+++ src/transformers/user_email_preferences/index.js
@@ -2,12 +2,16 @@ const BaseTransformer = require("./../BaseTransformer");
 
 class UserEmailPreferencesTransformer extends BaseTransformer {
     transform(userEmailPreferences) {
-        return {
-            id: userEmailPreferences && userEmailPreferences.id ? userEmailPreferences.id : false,
-            user_id: userEmailPreferences && userEmailPreferences.user_id ? userEmailPreferences.user_id : false,
-            global_disable: userEmailPreferences && userEmailPreferences.global_disable ? userEmailPreferences.global_disable : false,
-            claim_page_go_live: userEmailPreferences && userEmailPreferences.claim_page_go_live ? userEmailPreferences.claim_page_go_live : false,
-            outbid: userEmailPreferences && userEmailPreferences.outbid ? userEmailPreferences.outbid : false,
+        if(userEmailPreferences) {
+            return {
+                id: userEmailPreferences && userEmailPreferences.id,
+                user_id: userEmailPreferences && userEmailPreferences.user_id,
+                global_disable: userEmailPreferences && userEmailPreferences.global_disable,
+                claim_page_go_live: userEmailPreferences && userEmailPreferences.claim_page_go_live,
+                outbid: userEmailPreferences && userEmailPreferences.outbid,
+            }
+        } else {
+            return {}
         }
     }
 }
