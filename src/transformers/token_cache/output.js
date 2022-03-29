1d944f9ef77389902164549d149232edbd061ea2
Jay Welsh <thealchemicalopus@gmail.com>
2022-03-29 16:08:57 +0200

    chore(token_cache/output): include token_balance

--- src/transformers/token_cache/output.js
+++ src/transformers/token_cache/output.js
@@ -5,6 +5,7 @@ class TokenCacheOutputTransformer extends BaseTransformer {
         return {
             token_address: tokenCache.token_address,
             token_id: tokenCache.token_id,
+            token_balance: tokenCache.token_balance,
             token_holder: tokenCache.token_holder,
             metadata: JSON.parse(tokenCache.metadata),
         }
