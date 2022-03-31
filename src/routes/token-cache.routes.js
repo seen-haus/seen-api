f83f2bd52d0a769bacad3102f7a2a3602d3b9242
Jay Welsh <thealchemicalopus@gmail.com>
2022-03-31 20:11:00 +0200

    fix(token-cache.routes): fix route ordering

--- src/routes/token-cache.routes.js
+++ src/routes/token-cache.routes.js
@@ -5,8 +5,8 @@ const Router = require("./Router");
  * TokenCache
  */
 Router.get('/token-cache/',[],'TokenCacheController@index');
-Router.get('/token-cache/:tokenAddress/:holderAddress',[],'TokenCacheController@tokenCacheByTokenAndHolder');
 Router.get('/token-cache/holder/:holderAddress',[],'TokenCacheController@tokenCacheByHolder');
 Router.get('/token-cache/token/:tokenAddress',[],'TokenCacheController@tokenCacheByToken');
+Router.get('/token-cache/:tokenAddress/:holderAddress',[],'TokenCacheController@tokenCacheByTokenAndHolder');
 
 module.exports = Router.export();
