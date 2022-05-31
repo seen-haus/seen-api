c95f3b2d644a68ed6189fb28680c83cf47da88ea
Jay Welsh <thealchemicalopus@gmail.com>
2022-05-31 16:29:37 +0200

    fix(migrations): fix inconsistency with prod

--- src/database/migrations/20220504153201_token_cache_consignment_id.js
+++ src/database/migrations/20220504153201_token_cache_consignment_id.js
@@ -1,17 +1,12 @@
 const {TOKEN_CACHE_TABLE} = require("../../constants/DBTables");
 
-exports.up = async (knex) => {
-  await knex.schema.alterTable(TOKEN_CACHE_TABLE, (table) => {
-    table.dropColumn("consignment_id")
-  });
-  await knex.schema.alterTable(TOKEN_CACHE_TABLE, (table) => {
+exports.up = (knex) =>
+  knex.schema.alterTable(TOKEN_CACHE_TABLE, (table) => {
     table.integer("consignment_id")
-        .references("collectables.consignment_id")
         .unsigned()
         .nullable()
         .index();
   });
-}
 
 exports.down = (knex) =>
   knex.schema.alterTable(TOKEN_CACHE_TABLE, (table) => {
